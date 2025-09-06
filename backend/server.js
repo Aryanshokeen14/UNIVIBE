9d2394152096b1ede6bff8561d1e3d0ce7861e04
Aryanshokeen14 <aryanshokeen14@gmail.com>
2025-09-06 16:51:07 +0530

    width and height update in Image tag

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ import userRoutes from "./routes/user.routes.js";
 dotenv.config();
  
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://univibe-two.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(postRoutes);
 app.use(userRoutes);
